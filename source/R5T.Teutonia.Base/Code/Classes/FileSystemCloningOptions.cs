52c6d0af164fe2f93c217706b1a31ed810e5c06e
MinexAutomation <minex.automation@gmail.com>
2019-10-22 13:49:34 -0700

    Added options type for file-system cloning operations.

---
+++ source/R5T.Teutonia.Base/Code/Classes/FileSystemCloningOptions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace R5T.Teutonia
+{
+    public class FileSystemCloningOptions
+    {
+        public static readonly FileSystemCloningOptions Default = new FileSystemCloningOptions();
+
+
+        /// <summary>
+        /// Indicates that directories present in the destination, but not in the source, should be deleted.
+        /// Default: true.
+        /// </summary>
+        public bool DeleteExtraneousDestinationDirectories { get; set; } = true;
+        /// <summary>
+        /// Indicates that files present in the destination, but not in the source, should be deleted.
+        /// Default: true.
+        /// </summary>
+        public bool DeleteExtraneousDestinationFiles { get; set; } = true;
+    }
+}
