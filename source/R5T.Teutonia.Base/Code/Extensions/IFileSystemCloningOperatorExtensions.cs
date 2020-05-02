775a039f85de839b611aea9d9c124cbf9eeb2370
MinexAutomation <minex.automation@gmail.com>
2020-05-02 13:33:48 -0700

    Added IFileSystemCloningOperator extensions.

---
+++ source/R5T.Teutonia.Base/Code/Extensions/IFileSystemCloningOperatorExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using R5T.Gepidia;
+
+
+namespace R5T.Teutonia
+{
+    public static class IFileSystemCloningOperatorExtensions
+    {
+        /// <summary>
+        /// Clones the file-system source to the destination using <see cref="FileSystemCloningOptions.Default"/> options.
+        /// </summary>
+        public static void Clone(this IFileSystemCloningOperator fileSystemCloningOperator, FileSystemSite source, FileSystemSite destination)
+        {
+            fileSystemCloningOperator.Clone(source, destination, FileSystemCloningOptions.Default);
+        }
+    }
+}
