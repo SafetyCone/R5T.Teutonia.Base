4ec77f41903c86431f1896446914254aae1662f0
davidcoats <david@getsafetycone.com>
2022-04-27 15:37:02 -0700

    Interim changes.

---
+++ source/R5T.Teutonia.Base/Code/Services/Definitions/IFileSystemCloningOperator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+using R5T.Gepidia;
+
+using R5T.T0064;
+
+
+namespace R5T.Teutonia
+{
+    [ServiceDefinitionMarker]
+    public interface IFileSystemCloningOperator : IServiceDefinition
+    {
+        void Clone(FileSystemSite source, FileSystemSite destination, FileSystemCloningOptions options);
+        Task CloneAsync(FileSystemSite source, FileSystemSite destination, FileSystemCloningOptions options, CancellationToken cancellationToken);
+    }
+}
